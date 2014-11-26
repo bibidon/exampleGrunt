21a85e93020c468c114a739c038fb93218c913fe
bibidon <debibidon@gmail.com>
2014-11-27 01:25:21 +0300

    doma bad variant 27.11.2014

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,18 @@
 
     grunt.initConfig({
 
+        copy: {
+            dev: {
+                files: [
+                    { expand: true, cwd: 'bower_components/backbone/', src: 'backbone.js', dest: 'src/js/libs' },
+                    { expand: true, cwd: 'bower_components/jquery/dist/', src: 'jquery.js', dest: 'src/js/libs' },
+                    { expand: true, cwd: 'bower_components/requirejs/', src: 'require.js', dest: 'src/js/libs' },
+                    { expand: true, cwd: 'bower_components/underscore/', src: 'underscore.js', dest: 'src/js/libs' },
+                    { expand: true, cwd: 'bower_components/bootstrap/dist/css/', src: 'bootstrap.css', dest: 'src/css/libs' }
+                ]
+            }
+        },
+
         sass: {
             dev: {
                 files: {
@@ -24,7 +36,32 @@
                 }
             }
         }
-    });
 
+        //concat: {
+        //    options: {
+        //        separator: ';'
+        //    },
+        //    libs: {
+        //        src: 'bower_components'
+        //    }
+        //},
+
+        //watch: {
+        //    sass: {
+        //        files: ['sass/**/*.sass', 'sass/**/*.scss'],
+        //        task: 'sass'
+        //    },
+        //    js: {
+        //        files: ['js/**/*.js', '!js/main.build.js'],
+        //        task: 'requirejs'
+        //    }
+        //}
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    //grunt.loadNpmTasks('grunt-contrib-requirejs');
+    //grunt.loadNpmTasks('grunt-contrib-watch');
+
+   //grunt.registerTask('default', ['sass', 'requirejs']);
 }
