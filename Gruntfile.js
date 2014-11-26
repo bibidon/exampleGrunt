b3c1032adbac45458b24a2fb43c37de720671682
bibidon <debibidon@gmail.com>
2014-11-26 16:28:35 +0300

    on work 26.11.2014

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+﻿module.exports = function (grunt) {
+    'use strict';
+
+    grunt.initConfig({
+
+        sass: {
+            dev: {
+                files: {
+                    'css/main.css': 'sass/main.scss'
+                },
+                options: {
+                    compass: true,
+                    style: 'expanded',
+                    debugInfo: true
+                }
+            },
+            prod: {
+                files: {
+                    'css/main.build.css': 'sass/main.scss'
+                },
+                options: {
+                    compass: true,
+                    style: 'compressed'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+}
